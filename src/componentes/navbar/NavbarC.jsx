8e1bd48f8fbba228dfa2cd2a5706780711ef99c6
JLM224 <jorgemedina051001@gmail.com>
2025-10-02 15:02:51 -0300

    se crean navbar y pagina principal se mejora App.jsx con react router

---
+++ src/componentes/navbar/NavbarC.jsx
@@ -0,0 +1,26 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+
+const NavbarC = () => {
+  return (
+    <>
+    <Navbar expand="lg" className="navbar">
+      <Container>
+        <NavLink className="nav-link" to="/">
+        <img src="" alt="Logo"/>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <NavLink className='nav-link a' to="/">Inicio</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  )
+}
+
+export default NavbarC
