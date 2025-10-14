2e6917297884b762da06ba40722f175b93e56235
JLM224 <jorgemedina051001@gmail.com>
2025-10-14 14:02:17 -0300

    se mejora pagina principal

--- src/paginas/PaginaPrincipal.jsx
+++ src/paginas/PaginaPrincipal.jsx
@@ -1,10 +1,12 @@
 import CardsCA from '../componentes/cards/CardsCA'
 import Carrusel from '../componentes/carrusel/Carrusel'
+import TarjetasPrincipales from '../componentes/tarjetas/TarjetasPrincipales'
 
 const PaginaPrincipal = () => {
   return (
     <>
     <Carrusel/>
+    <TarjetasPrincipales/>
     <CardsCA/>
     </>
   )
